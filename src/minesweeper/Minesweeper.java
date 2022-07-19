1d0db861f9e59c8bdb801d65d212492a3da66360
Martina Zolcakova <zolcakova.martina@gmail.com>
2022-07-19 22:47:20 +0200

    domaca uloha 19.7.

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -7,21 +7,31 @@ import minesweeper.core.Field;
  * Main application class.
  */
 public class Minesweeper {
-    /** User interface. */
+    /**
+     * User interface.
+     */
     private UserInterface userInterface;
- 
+    private long startMillis;
+
     /**
      * Constructor.
      */
     private Minesweeper() {
         userInterface = new ConsoleUI();
-        
+        startMillis = System.currentTimeMillis();
+
         Field field = new Field(9, 9, 10);
         userInterface.newGameStarted(field);
     }
 
+    public int getPlayingSeconds() {
+        long currentTime = System.currentTimeMillis();
+        return (int) (currentTime-startMillis) / 1000;
+    }
+
     /**
      * Main method.
+     *
      * @param args arguments
      */
     public static void main(String[] args) {
